8524c813490603612651b1e7bc1dda23a4cbfb3b
Aditi0090 <165465425+Aditi0090@users.noreply.github.com>
2025-02-18 18:44:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     const output = document.getElementById("output");
 
-    output.innerHTML = `<tr><td colspan="2">Loading...</td></tr>`;
+    output.innerHTML = `<tr id="loading"><td colspan="2">Loading...</td></tr>`;
 
     function createPromise(index) {
-        let time = (Math.random() * 2 + 1).toFixed(3); 
+        let time = (Math.random() * 2 + 1).toFixed(3);
         return new Promise((resolve) => {
             setTimeout(() => resolve({ index, time }), time * 1000);
         });
     }
 
-    // Create 3 promises
     let promises = [createPromise(1), createPromise(2), createPromise(3)];
 
     Promise.all(promises).then((results) => {
-        output.innerHTML = "";
+        document.getElementById("loading").remove();
 
         let maxTime = 0;
 
         results.forEach((result) => {
             maxTime = Math.max(maxTime, parseFloat(result.time));
-            let row = `<tr><td>Promise ${result.index}</td><td>${result.time}</td></tr>`;
-            output.innerHTML += row;
+            output.innerHTML += `<tr><td>Promise ${result.index}</td><td>${result.time}</td></tr>`;
         });
 
-       
         output.innerHTML += `<tr><td>Total</td><td>${maxTime.toFixed(3)}</td></tr>`;
     });
 });
